bdad7a5428996f31c2386157021d0bbd1d136e8b
christianokuru <okuruchristian@gmail.com>
2025-05-06 16:30:07 +0100

    Created user and products modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsController } from './cats/cats.controller';
+import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, ProductsModule],
   controllers: [AppController, CatsController],
   providers: [AppService],
 })
--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,15 +1,16 @@
-import { Controller, Get, HttpCode, Param } from '@nestjs/common';
+import { Controller, Get, HttpCode, Param, Post, Redirect } from '@nestjs/common';
 
 @Controller('cats')
 export class CatsController {
     // /cats
     @Get()
-    scoobyDoo(): string {
-        return 'This action returns all cats from the database';
+    scoobyDoo(): string[] {
+        return ['This will return all cats from the database'];
     }
 
     // /breed
     @Get('breed')
+    @Redirect('https://nestjs.com', 301)
     getBreed(): string[] {
         return ['Persian', 'Maine Coon', 'Siamese'];
     }
@@ -18,7 +19,13 @@ export class CatsController {
     @Get(':id')
     // By deafault, the response from a route handler is 200 except it's a POST request which returns 201... but it can be changed usinfg the @HttpCode decorator
     // @HttpCode(201)
-    getCatById(@Param ('id') id: number): string {
+    getCatById(@Param (' ') id: number): string {
         return `This action returns a ${id} cat`;
+    }
+
+    // Post Handler
+    @Post()
+    createCat(): string {
+        return 'This adds a new cat!'
     }
 }
